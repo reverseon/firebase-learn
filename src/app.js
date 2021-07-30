e94b47be30d05ce8f6e2266eeee305f628c26e10
reverseon <reverseon@outlook.com>
2021-07-30 23:19:40 +0700

    validation

--- src/app.js
+++ src/app.js
@@ -7,18 +7,33 @@ chatroom.getChat(dat => chatUI.render(dat));
 const newchat = document.querySelector('.new-chat');
 const newname = document.querySelector('.new-name');
 const uhold = document.querySelector('.usernamehold');
+const errmsg = document.querySelector('.errormsg');
 uhold.innerText = username;
 
 newchat.addEventListener('submit', e=> {
     e.preventDefault();
-    chatroom.addChat(newchat.message.value).then(()=>newchat.reset()).catch(err=>console.log(err));
+    const ncval = newchat.message.value;
+    if (!/^[a-zA-Z ]{1,256}$/.test(ncval)) {
+        errmsg.parentElement.parentElement.classList.remove('d-none');
+        errmsg.innerHTML = `<b>Max 128 Character and ASCII recognized only</b>`
+    } else {
+        errmsg.parentElement.parentElement.classList.add('d-none')
+        chatroom.addChat(ncval).then(()=>newchat.reset()).catch(err=>console.log(err));
+    }
 })
 
 newname.addEventListener('submit', e => {
     e.preventDefault();
     const newuname = newname.uname.value.trim();
-    chatroom.updateName(newuname);
-    uhold.innerText = newuname;
+    if (!/^[a-z0-9]{1,16}$/.test(newuname)) {
+        errmsg.parentElement.parentElement.classList.remove('d-none');
+        errmsg.innerHTML = `<b>Lowercase and Number only max 16 character</b>`
+    } else {
+        errmsg.parentElement.parentElement.classList.add('d-none')
+        chatroom.updateName(newuname)
+        newname.reset();
+        uhold.innerText = newuname;
+    }
 })
 roombutton.addEventListener('click', e => {
     if (e.target.tagName === 'BUTTON') {
